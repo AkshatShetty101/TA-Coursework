b8f47ed22ea6ed439f3632a60edb0608868f872f
AkshatShetty101 <akshatshetty2908@gmail.com>
2018-09-02 13:10:46 +0530

    Finshed XML work

--- ADS/client/xml_helper.js
+++ ADS/client/xml_helper.js
@@ -1,97 +1,61 @@
-// var validator = require('xsd-schema-validator');
-
-function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      // addPerson(this);
-      // console.log(xmlhttp.responseXML);
-      addOrder(xmlhttp.responseXML, "abc@gmail.com", "2555");
-      parse(xmlhttp.responseXML);
-    }
-  };
-  xmlhttp.open("GET", "person_store.xml", true);
-  xmlhttp.send();
-}
-
-function validateXML(xml, xsd_path) {
-  var xmlText = new XMLSerializer().serializeToString(xml);
-  validator.validateXML(xmlText, xsd_path, function(err, result) {
-    if (err) {
-      throw err;
-    }
-    console.log(result); // true
+/**
+ *
+ * @param {*} file The path to the XML file
+ * @description Makes an XMLHttpRequest to get the XML data from the file path passed
+ * @returns The XML data of the file
+ */
+const loadXMLDoc = file =>
+  new Promise((resolve, reject) => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        resolve(xmlhttp.responseXML);
+      }
+    };
+    xmlhttp.open("GET", file, true);
+    xmlhttp.send();
   });
-}
 
-function parse(xml) {
-  var x, i, txt;
-  // validateXML(xml, 'person.xsd');
-  txt = "";
-  x = xml.getElementsByTagName("person");
-  for (i = 0; i < x.length; i++) {
-    var temp;
-    temp = x[i].getElementsByTagName("orders")[0];
-    // console.log(temp);
-    txt += temp.textContent + "<br>";
+/**
+ *
+ * @param {*} prefix The prefix to resolve
+ * @description The function takes a prefix and resolves it to the url to be passed
+ * @returns The appropriate url
+ */
+const nsResolver = prefix => {
+  switch (prefix) {
+    case "x":
+      return "https://www.w3schools.com";
+    case "mathml":
+      return "http://www.w3.org/1998/Math/MathML";
+    default:
+      return "http://example.com/domain";
   }
-  document.getElementById("demo").innerHTML = txt;
-}
+};
 
-function addAttribute(xml, element, key, value) {
-  var newElement = xml.createElement(key);
-  newElement.textContent = value;
-  element.appendChild(newElement);
-  return element;
-}
-
-function createPersonElement(xml) {
-  var newElement = xml.createElement("person");
-  newElement = addAttribute(xml, newElement, "email", "dasda@gmial.com");
-  newElement = addAttribute(xml, newElement, "name", "CDF");
-  newElement = addAttribute(xml, newElement, "password", "a");
-  newElement = addAttribute(xml, newElement, "orders", null);
-  return newElement;
-}
-
-function addPerson(xml) {
-  var x, newElement;
-  x = xml.getElementsByTagName("personstore")[0];
-  newElement = createPersonElement(xml);
-  x.appendChild(newElement);
-  console.log(x);
-}
-
-function nsResolver(prefix) {
-	console.log(prefix);
-	switch (prefix) {
-			case 'x':
-					return 'https://www.w3schools.com';
-			case 'mathml':
-					return 'http://www.w3.org/1998/Math/MathML';
-			default:
-					return 'http://example.com/domain';
-	}
-}
-
-function addOrder(xml, email, order) {
-  var x, newElement;
-  xpath = `//x:person[x:email='${email}']/x:orders`;
-  console.log(xpath);
-  x = xml.evaluate(
-		xpath,
-    xml,
-    nsResolver,
-    XPathResult.ANY_TYPE,
-    null
-  );
-  console.log(x);
-  x = x.iterateNext();
-  // while (result) {
-  //   console.log(result);
-  //   result = x.iterateNext();
-	// } 
-	newOrder = xml.createElement('order');
-  newOrder.textContent = order;
-	x.appendChild(newOrder);
-}
+/**
+ *
+ * @param {*} file The path to the XML file
+ * @param {*} xpath The Xpath query to be run
+ * @description Runs the xpath query and aggregates the result data in an array
+ * @returns An array containing the data queried
+ */
+const find = (file, xpath) =>
+  new Promise((resolve, reject) => {
+    loadXMLDoc(file).then(xml => {
+      const data = xml.evaluate(
+        xpath,
+        xml,
+        nsResolver,
+        XPathResult.ANY_TYPE,
+        null
+      );
+      let iterator = data.iterateNext();
+      const arr = [];
+      while (iterator) {
+        arr.push(iterator);
+        iterator = data.iterateNext();
+      }
+      resolve(arr);
+    });
+  });
